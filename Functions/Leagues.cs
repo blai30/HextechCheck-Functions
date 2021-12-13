45e1fcafc6ff9b788b2a3017d824d60843e01d33
Brian Lai <brianlai30@outlook.com>
2021-12-13 15:21:10 -0800

    feat: add leagues function

---
+++ Functions/Leagues.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using RiotSharp;
+using RiotSharp.Endpoints.LeagueEndpoint;
+using RiotSharp.Misc;
+
+namespace HextechCheck.Functions;
+
+public class Leagues
+{
+    private readonly ILogger _logger;
+    private readonly RiotApi _riotApi;
+
+    public Leagues(ILoggerFactory loggerFactory, RiotApi riotApi)
+    {
+        _logger = loggerFactory.CreateLogger<Leagues>();
+        _riotApi = riotApi;
+    }
+
+    [Function("Leagues")]
+    public async Task<List<LeagueEntry>?> Run(
+        [HttpTrigger(
+            AuthorizationLevel.Anonymous,
+            "get",
+            Route = "Leagues/{region}/{summonerId}")]
+        HttpRequestData req,
+        string region,
+        string summonerId,
+        FunctionContext executionContext)
+    {
+        if (string.IsNullOrEmpty(summonerId) || string.IsNullOrEmpty(region))
+        {
+            return null;
+        }
+
+        var regionEnum = Enum.Parse<Region>(region, true);
+        var leagues = await _riotApi.League.GetLeagueEntriesBySummonerAsync(regionEnum, summonerId);
+        return leagues;
+    }
+}
